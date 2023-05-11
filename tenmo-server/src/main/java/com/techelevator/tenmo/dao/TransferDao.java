11fef076429ffae28532ab6422595d9333eb4285
bwilbert67 <bwilbert67@gmail.com>
2023-05-11 11:27:58 -0400

    copied over my project

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -0,0 +1,20 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.model.Transfer;
+import org.postgresql.util.PSQLException;
+
+import java.util.List;
+
+public interface TransferDao {
+
+
+    Transfer createTransfer(Transfer transfer, int fromId, int toId) throws PSQLException;
+
+    List<Transfer> getAllTransfersByUserId (int userId);
+
+    Transfer viewTransferDetails (int id);
+
+    Transfer getTransferById(int id);
+
+    int updateTransfer(int id, int status);
+}
